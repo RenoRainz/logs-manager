d5c4284e81ebbde6668b60964fa7c081c8953785
RenoRainz <rainzreno@gmail.com>
2015-09-23 23:49:01 +0200

    Update code to sent each file to the bucket

--- log-sender.py
+++ log-sender.py
@@ -10,9 +10,9 @@
 import boto
 import sys
 import os
+import time
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
-from datetime import datetime
 
 # Global variables
 INPUT_QUEUE_NAME  = 'syslog-input'
@@ -37,24 +37,28 @@ def main(argv=None):
 	# Directory to watch
 	walk_dir = sys.argv[1]
 
-	# Browse into the directory
+	# TODO : Need to put this in infinite loop
+
+	# Browse into the directory and check when
+	# the file was modified, if > 1 we process it 
 	for root, subdirs, files in os.walk(walk_dir):
 		for name in files:
 			full_path = os.path.join(root, name)
-			print "full path : %s" % full_path
 			relative_path = os.path.relpath(full_path, walk_dir)
-			print "relative path : %s" % relative_path
+			last_modification = os.path.getmtime(full_path)
+			now = time.time()
+			delta = (int(now) - int(last_modification)) / 60
+			if delta > 1 :
+				print "send %s to bucket %s" % (full_path, BUCKET_NAME)
+				sent_to_s3(acces_key, secret_key, BUCKET_NAME, relative_path, full_path,False)
+				# TODO : Once file is sent we need to delete it
 
-	sent_to_s3(acces_key, secret_key, BUCKET_NAME, '/var/log/lastlog')
-
-def sent_to_s3(acces_key, secret_key, bucket_name, filename):
-
-	now = datetime.now()
+def sent_to_s3(acces_key, secret_key, bucket_name, key, filename, compress):
 
 	s3_conn = S3Connection(acces_key, secret_key)
 	bucket = s3_conn.get_bucket(bucket_name)
 	bucket_key = Key(bucket)
-	bucket_key.key = str(now.year) + "/" + str(now.month) + "/" + str(now.day) + "/" + str(now.hour) + "/" + os.path.basename(filename)
+	bucket_key.key = str(key)
 	sent_file = open(filename, 'r')
 	bucket_key.set_contents_from_file(sent_file, replace=True, rewind=True)	
 	sent_file.close()
