05a4d9e5aef4ae1af0315734e18bb6ad55ae10c2
RenoRainz <rainzreno@gmail.com>
2015-11-02 01:29:31 +0100

    Parse message and get value for bucket name and key

--- log-consumer.py
+++ log-consumer.py
@@ -15,16 +15,14 @@ import time
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 from boto.sqs.connection import SQSConnection
-#from boto.sqs import SQS
 import boto
-from boto.sqs.message import RawMessage
 import argparse
 import shutil
 import gzip
+import json
 
 # Global variables
 INPUT_QUEUE_NAME  = 'syslog-input'
-BUCKET_NAME = 'syslog-input'
 REGION = 'eu-west-1'
 CREDENTIALS_FILE = 'credentials/credentials.txt'
 
@@ -44,14 +42,23 @@ def main(argv=None):
 
     # TODO: put this block in try
 	sqs_conn = boto.sqs.connect_to_region(REGION, aws_access_key_id=acces_key, aws_secret_access_key=secret_key)
-	print sqs_conn
+	s3_conn = S3Connection(acces_key, secret_key)
 
     # Get queue
 	queue = sqs_conn.get_queue('syslog-input')
 
-	# Retrieve messages
-	retrieve_msg(acces_key, secret_key, sqs_conn, queue)
+    # Retrieve messages
+	msg = retrieve_msg(acces_key, secret_key, sqs_conn, queue)
 
+	if msg :
+		record = msg.get_body()
+		record_json = json.loads(record)
+
+		if 'Records' in record_json:
+			if 's3' in record_json['Records'][0]:
+				bucket = record_json['Records'][0]['s3']['bucket']['name']
+				s3_key =  record_json['Records'][0]['s3']['object']['key']
+				print "bucket : %s , key : %s" % (bucket, s3_key)
 
 def retrieve_msg(acces_key, secret_key, sqs_conn, queue):
 
@@ -59,11 +66,13 @@ def retrieve_msg(acces_key, secret_key, sqs_conn, queue):
 	Function to retrieve a msg from a SQS queue
 	"""
 
-	msg = sqs_conn.receive_message(queue, 5)
-
-	print "MSG : "
-	print type(msg)
-	print msg
+	msg = queue.read()
+	# Check if we get a message
+	if msg and len(msg) > 0:
+		return msg
+	else:
+		print "Queue empty"
+        return None
 
 
 
